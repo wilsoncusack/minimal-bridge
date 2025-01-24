af64f9c688c300ceb0a4ba73da3caae1771e166d
Tronica <wudmytrotest404@gmail.com>
2025-01-24 20:01:24 +0100

    fix typo simulateFinalizeWithdrawalTransaction.test.ts

--- src/actions/public/L1/simulateFinalizeWithdrawalTransaction.test.ts
+++ src/actions/public/L1/simulateFinalizeWithdrawalTransaction.test.ts
@@ -6,7 +6,7 @@ import { type FinalizeWithdrawalTransactionParameters } from '../../wallet/L1/wr
 import { simulateFinalizeWithdrawalTransaction } from './simulateFinalizeWithdrawalTransaction.js'
 
 // From https://etherscan.io/tx/0xcb571be93844895a45a4cf70cc4424fcc6ccf55dd4c14759da1efd57fa593ac5
-test('succesfully submits finalizeWithdrawalTransaction', async () => {
+test('successfully submits finalizeWithdrawalTransaction', async () => {
   const withdrawal: FinalizeWithdrawalTransactionParameters = {
     nonce: 1766847064778384329583297500742918515827483896875618958121606201292642114n,
     sender: '0x54392fc895e6e44538975272E0dD7335fCcC9045',
